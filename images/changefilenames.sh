d9d524a2893fef71e84154e8ba067071db536d7b
Jan Jiménez Serra <Jan.Jimenez.Serra@rabobank.nl>
2023-03-26 16:10:45 +0200

    v2

---
+++ images/changefilenames.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Change to the directory containing the files
+cd illustrations
+
+# Create a variable to keep track of the file number
+i=1
+
+# Loop through each file in the directory
+for file in *; do
+  # Check if the file is a regular file (not a directory)
+  if [ -f "$file" ]; then
+    # Create a new filename with the format "numbered-list-x"
+    new_name="illustrations$i"
+    
+    # Append the file extension to the new filename
+    extension="${file##*.}"
+    new_name="$new_name.$extension"
+    
+    # Rename the file
+    mv "$file" "$new_name"
+    
+    # Increment the file number
+    i=$((i+1))
+  fi
+done
